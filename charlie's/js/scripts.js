a4c52146e29bf0940a646c60a9f2b9ee3a3d5311
ammore17 <97985440+ammore17@users.noreply.github.com>
2022-03-15 19:10:25 -0700

    chnge

---
+++ charlie's/js/scripts.js
@@ -0,0 +1,23 @@
+var slider = tns({
+    container: '.hotdrinks',
+    nav: false,
+    items: 3,
+    slideBy: 'page',
+    mouseDrag: true,
+  });
+
+  var slider = tns({
+    container: '.sandwiches',
+    nav: false,
+    items: 3,
+    slideBy: 'page',
+    mouseDrag: true,
+  });
+
+  var slider = tns({
+    container: '.iceddrinks',
+    nav: false,
+    items: 3,
+    slideBy: 'page',
+    mouseDrag: true,
+  });
